f22dd7483c3e61cfa6fa9df3d5176f120010f2f9
Wangjiazhen <841713301@qq.com>
2021-12-14 15:27:36 +0800

    [Fix] fix check copyright and add unit test

--- mmprecommit/check_copyright.py
+++ mmprecommit/check_copyright.py
@@ -10,100 +10,108 @@ HEADER = 'Copyright (c) OpenMMLab. All rights reserved.\n'
 HEADER_KEYWORDS = {'Copyright', 'License'}
 
 
-def is_contain_header(lines: List[str]) -> bool:
+def has_copyright(lines: List[str]) -> bool:
     for line in lines:
-        if len(HEADER_KEYWORDS.intersection(set(line.split(' ')))):
+        if not HEADER_KEYWORDS.isdisjoint(set(line.split(' '))):
             return True
     return False
 
 
 def parse_args():
-    parser = argparse.ArgumentParser(description='Add header to files')
-    parser.add_argument('src', type=str, help='source files to add header')
+    parser = argparse.ArgumentParser(description='Add copyright to files')
     parser.add_argument(
-        '--exclude_dirs', nargs='*', type=str, default=[], help='exclude folder')
+        'include', type=str, nargs='+', help='directory to add copyright')
     parser.add_argument(
-        '--include_suffixes',
-        nargs='+',
+        '--exclude', nargs='*', type=str, default=[], help='exclude directory')
+    parser.add_argument(
+        '--suffix',
+        nargs='*',
         type=str,
         default=['.py'],
-        help='header will be added to files with suffix')
+        help='copyright will be added to files with suffix')
     args = parser.parse_args()
     return args
 
 
-def format_args(args):
-    r"""Check Check the correctness of args and format them.
-    """
+def check_args(args) -> int:
+    """Check the correctness of args and format them."""
 
-    suffix_list = ['.py', '.h', '.cpp', '.cu', '.cuh', '.hpp']
+    suffixes = ['.py', '.h', '.cpp', '.cu', '.cuh', '.hpp']
 
     # remove possible duplication
-    args.exclude_dirs = list(set(args.exclude_dirs))
-    args.include_suffixes = list(set(args.include_suffixes))
+    args.include = list(set(args.include))
+    args.exclude = list(set(args.exclude))
+    args.suffix = list(set(args.suffix))
 
     # check the correctness and format args
-    if not isinstance(args.src, str) or not osp.exists(args.src):
-        raise ValueError(f"src {args.src} is not a right path")
-    else:
-        args.src = osp.abspath(args.src)
-    for i, dir in enumerate(args.exclude_dirs):
-        if not isinstance(dir, str) or not osp.exists(dir):
-            raise ValueError(f"{dir} in exclude_dirs is not a right path")
+    for i, dir in enumerate(args.include):
+        if not osp.exists(dir):
+            raise FileNotFoundError(f"Include {dir} can not be found")
         else:
-            args.exclude_dirs[i] = osp.abspath(dir)
-    for suffix in args.include_suffixes:
-        if suffix not in suffix_list:
-            raise ValueError(
-                f"{suffix} in include_suffixes is not a right suffix")
-    return args
-
-
-def get_filepath_list(args) -> List[str]:
-    r"""Get all file paths that match the args.
-    """
-
-    filepath_list = []
-    for root, dirs, files in os.walk(args.src):
-        is_exclude = False
-        for dir in args.exclude_dirs:
-            if root.startswith(dir):
-                is_exclude = True
-                break
-        if is_exclude:
-            continue
+            args.include[i] = osp.abspath(dir)
+    for i, dir in enumerate(args.exclude):
+        if not osp.exists(dir):
+            raise FileNotFoundError(f"Exclude {dir} can not be found")
         else:
-            for file in files:
-                for suffix in args.include_suffixes:
-                    if file.endswith(suffix):
-                        filepath = osp.join(root, file)
-                        filepath_list.append(filepath)
-                        break
-    return filepath_list
+            args.exclude[i] = osp.abspath(dir)
+    for suffix in args.suffix:
+        if suffix not in suffixes:
+            raise FileNotFoundError(f"Suffix {suffix} can not be found")
 
 
-def add_copyright(filepath_list: List[str]):
-    r"""Add copyright for files which lack copyright
+def get_filepaths(args) -> List[str]:
+    """Get all file paths that match the args."""
+
+    filepaths = []
+    for dir in args.include:
+        for root, dirs, files in os.walk(dir):
+            is_exclude = False
+            for dir in args.exclude:
+                if root.startswith(dir):
+                    is_exclude = True
+                    break
+            if is_exclude:
+                continue
+            else:
+                for file in files:
+                    for suffix in args.suffix:
+                        if file.endswith(suffix):
+                            filepath = osp.join(root, file)
+                            filepaths.append(filepath)
+                            break
+    return filepaths
+
+
+def check_copyright(filepaths: List[str]) -> int:
+    """Add copyright for those files which lack copyright.
+    
+    Args:
+       filepaths (list[str]): File paths to be checked.
+    
+    returns:
+        int: Returns 0 if no file is missing copyright, otherwise returns 1.
     """
-    for filepath in filepath_list:
-        with open(filepath, 'r') as f:
+    rev = 0
+    for filepath in filepaths:
+        with open(filepath, 'r', encoding='utf-8') as f:
             lines = f.readlines()
-        if not is_contain_header(lines):
-            with open(filepath, 'w') as f:
+        if not has_copyright(lines):
+            with open(filepath, 'w', encoding='utf-8') as f:
                 prefix = '# ' if osp.splitext(filepath)[1] == '.py' else '// '
                 f.writelines([prefix + HEADER] + lines)
+                rev = 1
+    return rev
 
 
 def main():
     args = parse_args()
     try:
-        args = format_args(args)
-    except ValueError as e:
+        check_args(args)
+    except FileNotFoundError as e:
         print(repr(e))
         return 1
-    filepath_list = get_filepath_list(args)
-    add_copyright(filepath_list)
-    return 0
+    filepaths = get_filepaths(args)
+    return check_copyright(filepaths)
 
 
 if __name__ == '__main__':
---
+++ tests/test_copyright.py
@@ -0,0 +1,20 @@
+import os
+import os.path as osp
+
+
+def test_copyright():
+
+    include = "./tests/data"
+    exclude = "./tests/data/exclude"
+
+    for root, dirs, files in os.walk(include):
+        for file in files:
+            filepath = osp.join(root, file)
+            with open(filepath, 'r+', encoding='utf-8') as f:
+                lines = f.readlines()
+                if root != exclude:
+                    assert lines[0].split(' ').count("Copyright") > 0
+                    f.seek(0, 0)
+                    f.writelines(lines[1:])
+                else:
+                    assert lines[0].split(' ').count("Copyright") == 0
